d123d5b7eb127f4404e85e50b8d1ecad23ba63ba
Kristian Elset Boe <kristian.e.boe@gmail.com>
2019-01-19 15:13:09 +0100

    Updates

---
+++ precache-manifest.3a791418b2fabba2381220fa1b3a9eb3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a315ea8e51f2195a8d63bee7c2b26dbc",
+    "url": "/static/media/yaps.a315ea8e.jpg"
+  },
+  {
+    "revision": "b374c25be7c2da11b69a",
+    "url": "/static/css/main.45940029.chunk.css"
+  },
+  {
+    "revision": "8fdff909f31d2416e22b",
+    "url": "/static/js/1.8fdff909.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f232a37b83f8d31432c4607d7e29fd49",
+    "url": "/static/media/kristianeboe.f232a37b.jpg"
+  },
+  {
+    "revision": "b374c25be7c2da11b69a",
+    "url": "/static/js/main.b374c25b.chunk.js"
+  },
+  {
+    "revision": "c3d5bf21410fff42332281ff9ede6c3e",
+    "url": "/static/media/linkedIn.c3d5bf21.jpg"
+  },
+  {
+    "revision": "e86753f52f5a5345983250beb8a1cf71",
+    "url": "/static/media/blunk.e86753f5.jpg"
+  },
+  {
+    "revision": "a3f1e612f7546885cdbd4f313cc877bf",
+    "url": "/static/media/coding.a3f1e612.jpg"
+  },
+  {
+    "revision": "5fa790db4ca4d50250a60129d7a62ac2",
+    "url": "/static/media/boostamins.5fa790db.jpg"
+  },
+  {
+    "revision": "3d8c40dce40065bc71cd1e9bd5560d07",
+    "url": "/index.html"
+  }
+];
